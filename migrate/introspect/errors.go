a3c66b4a8a850d83744e0d988588218212bf0a3c
Satish Babariya <satish.babariya@gmail.com>
2025-12-28 11:54:11 +0530

    chore: Delete the entire prisma-go project codebase, including PSL, migration, query, CLI, and test components.

--- migrate/introspect/errors.go
+++
@@ -1,9 +0,0 @@
-package introspect
-
-import "errors"
-
-var (
-	ErrUnsupportedProvider = errors.New("unsupported database provider")
-	ErrConnectionFailed    = errors.New("failed to connect to database")
-	ErrIntrospectionFailed = errors.New("database introspection failed")
-)
--- query/compiler/errors.go
+++
@@ -1,9 +0,0 @@
-package compiler
-
-import "errors"
-
-var (
-	ErrUnsupportedQuery  = errors.New("unsupported query type")
-	ErrInvalidQuery      = errors.New("invalid query")
-	ErrCompilationFailed = errors.New("query compilation failed")
-)
